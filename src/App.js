237aaad069ef393164316484d6ccac3d921b3606
German B <geuxor@dr.com>
2021-10-30 00:15:13 +0200

    feat-disable Enter key

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import AppRouter from "./router/AppRouter";
 
 const App = () => {
   console.log(React.version);
+  window.addEventListener('keydown', function (e) {
+    if (e.code === 'Enter' || e.keyIdentifier === 'U+000A' || e.keyIdentifier === 'Enter' || e.key === 13) {
+      if (e.target.nodeName === 'INPUT' && e.target.type === 'text') {
+        e.preventDefault(); return false;
+      }
+    }
+  }, true);
+
   return (
     <div className="">
       <AppRouter />
