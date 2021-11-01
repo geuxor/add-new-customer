6f71c625346c25684e480b01c24bf2bf0c8ba8f6
German B <geuxor@dr.com>
2021-11-01 22:04:30 +0100

    data-modified and complete

--- src/data/questionsListData.js
+++ src/data/questionsListData.js
@@ -8,40 +8,40 @@ const questionsList = [
     headline: "Welcome",
     title: "You need to upload and completeKYC & Due Diligence. Input your basic information about the customer and add the UBO. And upload KYC documents and make Due diligence.",
   },
-  // {
-  //   id: 2,
-  //   headline: "Company Details",
-  //   title: "What is the maximum file upload size?",
-  //   inputs: [
-  //     { label: "Company", name: "company", placeholder: "", type: "text", required: true, error: "Company is required" },
-  //     { label: "Country", name: "country", placeholder: "", type: "select", options: countryList, required: true, error: "Country is required" },
-  //     { label: "Address", name: "address", placeholder: "", type: "text", required: true, error: "Address is required" },
-  //     { label: "Apartment, suit, etc.", name: "apartment", placeholder: "", type: "text", required: false },
-  //     { label: "Postal code/zip", name: "postcode", placeholder: "", type: "text", required: true, error: "Postal code/zip" },
-  //     { label: "City", name: "city", placeholder: "", type: "text", required: true, error: "City is required" },
-  //     { label: "Website", name: "website", placeholder: "", type: "text", required: false },
-  //     { label: "Company registration from official register", name: "registration-nr", placeholder: "", type: "text", required: true, error: "Company registration is required" }
-  //   ]
-  // },
-  // {
-  //   id: 3,
-  //   headline: "Ultimate Beneficial Owner(s)",
-  //   title: "Who is the Ultimate Beneficial Owner(s)?",
-  //   description: "UBO of a legal entity are the natural persons who directly or indirectly hold or control a stake of at least 25% in the capital or of at least 25% of th evoting rights of the company, or who undertake the de jure or de facto management of the legal entity.",
-  //   subdescription: "Details of the person(s) acting as a representative(s) (the undersigned)",
-  //   inputs: [
-  //     { id: 1, label: "Full name", name: "full-name", placeholder: "", type: "text", required: true, error: "Full name is required" },
-  //     { id: 2, label: "Role", name: "role", placeholder: "", type: "select", options: roleList, required: true, error: "Role is required" },
-  //     { id: 3, label: "Address", name: "address", placeholder: "", type: "text", required: true, error: "Address is required" },
-  //     { id: 4, label: "Apartment, suit, etc.", name: "apartment", placeholder: "", type: "text", required: false },
-  //     { id: 5, label: "Postal code/zip", name: "postcode", placeholder: "", type: "text", required: true, error: "Postal code/zip" },
-  //     { id: 6, label: "City", name: "city", placeholder: "", type: "text", required: true, error: "City is required" },
-  //     { id: 7, label: "Email", name: "email", placeholder: "", type: "email", required: true, error: "Email is required" },
-  //     { id: 8, label: "Upload Passport on UBO", name: "passport", placeholder: "e.g. PNG, JPG- file size max 1mb", type: "file", required: false, error: "Passport is required" },
-  //     { id: 9, label: "Upload Utility bill on UBO", name: "utility", placeholder: "e.g. PNG, JPG- file size max 1mb", type: "file", required: false, error: "Utility Bill is required" }
-  //   ],
-  //   allowMultiple: true
-  // },
+  {
+    id: 2,
+    headline: "Company Details",
+    title: "What is the maximum file upload size?",
+    inputs: [
+      { label: "Company", name: "company", placeholder: "", type: "text", required: true, error: "Company is required" },
+      { label: "Country", name: "country", placeholder: "", type: "select", options: countryList, required: true, error: "Country is required" },
+      { label: "Address", name: "address", placeholder: "", type: "text", required: true, error: "Address is required" },
+      { label: "Apartment, suit, etc.", name: "apartment", placeholder: "", type: "text", required: false },
+      { label: "Postal code/zip", name: "postcode", placeholder: "", type: "text", required: true, error: "Postal code/zip" },
+      { label: "City", name: "city", placeholder: "", type: "text", required: true, error: "City is required" },
+      { label: "Website", name: "website", placeholder: "", type: "text", required: false },
+      { label: "Company registration from official register", name: "registration-nr", placeholder: "", type: "text", required: true, error: "Company registration is required" }
+    ]
+  },
+  {
+    id: 3,
+    headline: "Ultimate Beneficial Owner(s)",
+    title: "Who is the Ultimate Beneficial Owner(s)?",
+    description: "UBO of a legal entity are the natural persons who directly or indirectly hold or control a stake of at least 25% in the capital or of at least 25% of th evoting rights of the company, or who undertake the de jure or de facto management of the legal entity.",
+    subdescription: "Details of the person(s) acting as a representative(s) (the undersigned)",
+    inputs: [
+      { id: 1, label: "Full name", name: "full-name", placeholder: "", type: "text", required: true, error: "Full name is required" },
+      { id: 2, label: "Role", name: "role", placeholder: "", type: "select", options: roleList, required: true, error: "Role is required" },
+      { id: 3, label: "Address", name: "address", placeholder: "", type: "text", required: true, error: "Address is required" },
+      { id: 4, label: "Apartment, suit, etc.", name: "apartment", placeholder: "", type: "text", required: false },
+      { id: 5, label: "Postal code/zip", name: "postcode", placeholder: "", type: "text", required: true, error: "Postal code/zip" },
+      { id: 6, label: "City", name: "city", placeholder: "", type: "text", required: true, error: "City is required" },
+      { id: 7, label: "Email", name: "email", placeholder: "", type: "email", required: true, error: "Email is required" },
+      { id: 8, label: "Upload Passport on UBO", name: "passport", placeholder: "e.g. PNG, JPG- file size max 1mb", type: "file", required: false, error: "Passport is required" },
+      { id: 9, label: "Upload Utility bill on UBO", name: "utility", placeholder: "e.g. PNG, JPG- file size max 1mb", type: "file", required: false, error: "Utility Bill is required" }
+    ],
+    allowMultiple: true
+  },
   {
     id: 4,
     headline: "Nature of business",
--- src/data/roleListData.js
+++ src/data/roleListData.js
@@ -2,6 +2,6 @@ const roleList = [
   { code: "MNG", code3: "MNGMT", name: "Manager", number: "001" },
   { code: "FND", code3: "FOUND", name: "Founder", number: "002" },
   { code: "OWN", code3: "OWNER", name: "Owner", number: "003" },
-  { code: "UBO", code3: "UBOWN", name: "ULtimate Beneficial Owner", number: "004" },
+  { code: "UBO", code3: "UBOWN", name: "Ultimate Beneficial Owner", number: "004" },
 ]
 export default roleList;
